9f23a1076bb8913308b025a4ac0c2342e4aae7a8
zhaojiahua <53359717+zhaojiahua@users.noreply.github.com>
2025-01-15 10:06:43 +0800

    Add files via upload

---
+++ WXProject/Source/WXProject/WXProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WXProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WXProject, "WXProject" );
