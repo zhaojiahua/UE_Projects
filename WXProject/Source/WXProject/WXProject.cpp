cec4ad115206b8d1d1dddf4df1ec70ecf5b5ee3e
zhaojiahua <hua4237@foxmail.com>
2025-05-20 12:07:23 +0800

    删除Source文件夹

--- WXProject/Source/WXProject/WXProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "WXProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WXProject, "WXProject" );
