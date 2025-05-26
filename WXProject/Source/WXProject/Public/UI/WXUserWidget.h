06488d59255ee47c49eb9cff6290eece2d7c99f7
zhaojiahua <hua4237@foxmail.com>
2025-05-26 13:37:17 +0800

    制作了简易的血条和蓝条

---
+++ WXProject/Source/WXProject/Public/UI/WXUserWidget.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "WXUserWidget.generated.h"
+
+
+UCLASS()
+class WXPROJECT_API UWXUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
